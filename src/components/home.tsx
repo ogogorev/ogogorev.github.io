6965727c43baa94b392c326d9abe0ba0ea0522dd
Gorev <ogogorev@yandex.ru>
2020-06-24 18:25:14 +0200

    Add home and project pages drafts

---
+++ src/components/home.tsx
@@ -0,0 +1,16 @@
+import JSX from '../helpers/jsx'
+
+export default function Home() {
+  const container = (
+    <div>
+      Home page
+
+      Links:
+      <ul>
+        <li><a href="/projects">Projects</a></li>
+      </ul>
+    </div>
+  )
+
+  return container
+}
---
+++ src/components/projects.tsx
@@ -0,0 +1,7 @@
+import JSX from '../helpers/jsx'
+
+export function Projects() {
+  return (
+    <div>hello from projects</div>
+  )
+}
