e61a1743ead12cf484f036074957827af736f5b2
Gorev <ogogorev@yandex.ru>
2020-07-08 21:57:58 +0200

    Handle tags from svg namespace

--- src/helpers/jsx.ts
+++ src/helpers/jsx.ts
@@ -1,3 +1,7 @@
+const SVG_TAGS = [
+  'svg', 'path', 'g', 'animate'
+]
+
 function createElement(tagName, attrs = {}, ...children) {
   attrs = attrs || {}
 
@@ -5,7 +9,14 @@ function createElement(tagName, attrs = {}, ...children) {
     return createComponent(tagName, attrs, children)
   }
 
-  const elem = Object.assign(document.createElement(tagName), attrs)
+  let elem
+
+  if (isSVGTag(tagName)) {
+    elem = createSVGElement(tagName, attrs)
+  } else {
+    elem = Object.assign(document.createElement(tagName), attrs)
+  }
+
   for (const child of children) {
     if (Array.isArray(child)) elem.append(...child)
     else elem.append(child)
@@ -17,6 +28,18 @@ function createElement(tagName, attrs = {}, ...children) {
 function createComponent(tagName, attrs = {}, ...children) {
   const node = tagName({ ...attrs, children })
   return node
+}
+
+function isSVGTag(tagName) {
+  return SVG_TAGS.includes(tagName)
+}
+
+function createSVGElement(tagName, attrs) {
+  const elem: HTMLElement = document.createElementNS("http://www.w3.org/2000/svg", tagName)
+  for (const a in attrs) {
+    elem.setAttribute(a, attrs[a])
+  }
+  return elem
 }
 
 const JSX = {
