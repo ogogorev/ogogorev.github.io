9674f78cff6e88540ef9e031848072aebf79c9fb
Gorev <ogogorev@yandex.ru>
2020-07-12 10:19:10 +0200

    Show toggle-button in nav

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -8,18 +8,33 @@ export default function Navigation() {
 
   const onMenuButtonToggle = (isOpened) => {
     console.log('on toggle', isOpened)
+    toggleNavigation(isOpened)
   }
 
-  const root = (
+  const openedClassName = 'opened'
+  const toggleNavigation = (isOpened) => {
+    (isOpened) ? root.classList.add(openedClassName) : root.classList.remove(openedClassName)
+  }
+
+  const root: HTMLElement = (
     <div className="navigation">
-      <ToggleMenuButton onToggle={onMenuButtonToggle} />
-      <div className="navigation__menu">
-        <Link href="/" className="navigation__link">HOME</Link>
-        <Link href="/projects" className="navigation__link">PROJECTS</Link>
-        <Link href="/about" className="navigation__link">ABOUT ME</Link>
+      <div className="navigation__placeholder"></div>
+      <div className="navigation__content">
+        <ToggleMenuButton onToggle={onMenuButtonToggle} />
+        <div className="navigation__menu">
+          <Link href="/" className="navigation__link">HOME</Link>
+          <Link href="/projects" className="navigation__link">PROJECTS</Link>
+          <Link href="/about" className="navigation__link">ABOUT ME</Link>
+        </div>
       </div>
     </div>
   )
+
+  // const s = getComputedStyle(root)
+
+  // console.log('ROOT', s.transform, s.webkitTransform)
+
+  // root.style.top = window.innerHeight + 'px'
 
   return root
 }
