69807f5eb29c08cc72fe5bea157df29e5a02c726
Gorev <ogogorev@yandex.ru>
2020-07-12 14:46:37 +0200

    Update styles on project page

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,29 +1,38 @@
 import JSX from '../../helpers/jsx'
+import { navigate } from '../../helpers/router'
+
+import './projects.css'
 
 function Card({ data }) {
   const { title, desc } = data
 
   const root = (
-    <div>
-      <h3>{title}</h3>
-      <p>{desc}</p>
+    <div className="project-card">
+      <h3 className="project-card__title">{title}</h3>
+      <p className="project-card__desc">{desc}</p>
     </div>
   )
   return root
 }
 
 const data = [
-  { title: 'Awesome project', desc: 'sdljfndlsf ljdlfk jsdkljf sdf' },
-  { title: 'Another Project', desc: 'gukwefiojfojeroijfe v8we7f98we7' },
-  { title: 'this website', desc: 'sdljfndlsf ljdlfk jsdkljf sdf' },
+  { path: 'project-1', title: 'Awesome project', desc: 'sdljfndlsf ljdlfk jsdkljf sdf' },
+  { path: 'project-2', title: 'Another Project', desc: 'gukwefiojfojeroijfe v8we7f98we7' },
+  { path: 'project-3', title: 'this website', desc: 'sdljfndlsf ljdlfk jsdkljf sdf' },
 ]
 
 export default function Projects() {
+
+  function handleCardClick(path: string) {
+    // TODO: Use path here for navigation
+    navigate('/')
+  }
+
   return (
     <div>
-      <ul className="projects-list">
+      <ul className="project-list">
         {data.map(e => (
-          <li className="projects-list__item">
+          <li className="project-list__item" onclick={() => handleCardClick(e.path)}>
             <Card data={e} />
           </li>
         ))}
