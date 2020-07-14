d8ebc7941d709018702afbb12676ffa191aa4954
Gorev <ogogorev@yandex.ru>
2020-07-14 21:49:54 +0200

    Get project list from state, wrap project cards in Links

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,39 +1,32 @@
 import JSX from '../../helpers/jsx'
-import { navigate } from '../../helpers/router'
+import { Link } from '../../helpers/router'
+import state from '../../state/state'
 
 import './projects.css'
 
 function Card({ data }) {
-  const { title, desc } = data
+  const { title, text } = data
 
   const root = (
-    <div className="project-card">
-      <h3 className="project-card__title">{title}</h3>
-      <p className="project-card__desc">{desc}</p>
-    </div>
+    <Link href={`/projects/dsfsdf`} className="project-card-container">
+      <div className="project-card">
+        <h3 className="project-card__title">{title}</h3>
+        <p className="project-card__desc">{text}</p>
+      </div>
+    </Link>
   )
   return root
 }
 
-const data = [
-  { path: 'project-1', title: 'Awesome project', desc: 'sdljfndlsf ljdlfk jsdkljf sdf' },
-  { path: 'project-2', title: 'Another Project', desc: 'gukwefiojfojeroijfe v8we7f98we7' },
-  { path: 'project-3', title: 'this website', desc: 'sdljfndlsf ljdlfk jsdkljf sdf' },
-]
-
 export default function Projects() {
-
-  function handleCardClick(path: string) {
-    // TODO: Use path here for navigation
-    navigate('/')
-  }
+  const projects = state.get('projects')
 
   return (
     <div>
       <ul className="project-list">
-        {data.map(e => (
-          <li className="project-list__item" onclick={() => handleCardClick(e.path)}>
-            <Card data={e} />
+        {projects.map(p => (
+          <li className="project-list__item">
+            <Card data={p} />
           </li>
         ))}
       </ul>
