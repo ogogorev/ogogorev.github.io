40c460e72c008719bcf87a6f0fdd720d7953925d
Gorev <ogogorev@yandex.ru>
2020-07-21 21:10:29 +0200

    Add sass-loader

--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
