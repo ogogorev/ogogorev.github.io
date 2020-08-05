c9456a9b6248ca9ce89f4aa0335b2f0a05d8c68c
Gorev <ogogorev@yandex.ru>
2020-08-05 20:34:06 +0200

    Add external links to the footer, rename footer.css to scss, update styles

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,17 +1,25 @@
 import JSX from '../../helpers/jsx'
 import { Link } from '../../helpers/router'
+import githubIconSrc from '../../assets/img/github.svg'
+import linkedInIconSrc from '../../assets/img/linkedin.svg'
 
-import './footer.css'
+import './footer.scss'
 
 export default function Footer() {
-
   const root: HTMLElement = (
     <div className="footer">
       <div className="footer__content">
-        <div className="footer__menu">
-          <Link href="/about" className="footer__link">ABOUT ME</Link>
-          <div className="footer__icons-container">
-            icons
+        <div className="footer__menu margin">
+          <Link href="/about" className="footer__link">
+            ABOUT ME
+          </Link>
+          <div className="footer__external-links-container">
+            <a href="https://github.com/ogogorev" target="_blank">
+              <img src={githubIconSrc} alt="" />
+            </a>
+            <a href="https://de.linkedin.com/" target="_blank">
+              <img src={linkedInIconSrc} alt="" />
+            </a>
           </div>
         </div>
       </div>
