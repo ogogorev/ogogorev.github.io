d2dd964c61cca6a7a43e4cf763db2dddac4744fc
Gorev <ogogorev@yandex.ru>
2020-09-12 10:46:12 +0200

    Update content and adjust styles on home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,18 @@
 import JSX from '../../helpers/jsx'
-
 import { Link } from '../../helpers/router'
+
+import './home.scss'
 
 export default function Home() {
   const container: HTMLElement = (
-    <div className="margin">
-      <h1 style="margin-top: 0">Roman Gorev</h1>
+    <div className="home-page margin">
+      <h1 classnName="title">Roman Gorev</h1>
 
       <p>
         Front-end developer, based in Frankfurt, Germany. <br />
-        Currently working at Accenture. <br />
-        Feel free to explore my <Link href="/projects">projects</Link>.
+        Currently working at Accenture. <br /> <br />
+        Don't hesitate to take a look at my{' '}
+        <Link href="/projects">projects</Link>.
       </p>
     </div>
   )
